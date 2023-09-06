f439dac2051130942cec07953815bf1553434896
Nishanth <nisanthrtts@gmail.com>
2023-09-06 12:37:06 +0530

    server url updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,34 @@
-import { useState } from 'react';
+import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import axios from 'axios'
-import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero";
-import BikeDetails from './pages/BikeDetails';
-import Form from './pages/Form';
+import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero'
+import BikeDetails from './pages/BikeDetails'
+import Form from './pages/Form'
 
 import { BikeContext } from './Context/BikeContext'
 
 function App() {
-  axios.defaults.baseURL = 'https://bike-rental-yftg.onrender.com/api/v1'
+  axios.defaults.baseURL = 'https://itchy-dog-polo-shirt.cyclic.app/api/v1'
   axios.defaults.headers = {
     'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
   }
 
   const [bikeid, setBikeid] = useState('')
-  
+
   return (
     <div>
-      <BikeContext.Provider value={{bikeid, setBikeid}}>
-        <Navbar/> 
+      <BikeContext.Provider value={{ bikeid, setBikeid }}>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Hero/>}/>
-          <Route path='/ride-details' element={<BikeDetails/>}/>
-          <Route path='/host-bike' element={<Form/>}/>
+          <Route path='/' element={<Hero />} />
+          <Route path='/ride-details' element={<BikeDetails />} />
+          <Route path='/host-bike' element={<Form />} />
         </Routes>
       </BikeContext.Provider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
